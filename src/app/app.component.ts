31e6c27e7fa92b48bc8f95c40d3ff787391609ea
nilss <nils.stenholm@gmail.com>
2016-05-15 01:36:44 +0300

    github auth 0.1 and navbar-stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ROUTER_PROVIDERS, Routes } from '@angular/router';
+import { CORE_DIRECTIVES } from '@angular/common';
 
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { AngularFire } from 'angularfire2';
 
 import {NavbarComponent} from './navbar/navbar.component';
 
 @Component({
   selector: 'rh-app',
-  template: '<rh-navbar></rh-navbar>',
+  template: `
+  <rh-navbar></rh-navbar>
+  <router-outlet></router-outlet>
+  `,
   directives: [NavbarComponent],
   providers : [AngularFire]
 })
-
+@Routes([
+  // {path: '/profile',       component: ProfileComponent},
+  // {path: '/feed',          component: ReviewFeedComponent},
+  // {path: '/feed/:id',      component: HeroDetailComponent}
+])
 export class AppComponent implements OnInit{
 
-  constructor(private _af : AngularFire){
+  constructor(){
   }
 
   ngOnInit(){
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { CORE_DIRECTIVES } from '@angular/common';
 
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'rh-navbar',
-  templateUrl: '/app/navbar/navbar.Component.html'
+  templateUrl: '/app/navbar/navbar.Component.html',
+  directives: [CORE_DIRECTIVES]
 })
-
 export class NavbarComponent implements OnInit{ 
 
-  constructor(private _af : AngularFire){
+  public auth : any;
 
+  constructor(public af : AngularFire){
+    this.af.auth.subscribe((auth) => {
+      console.log(auth)
+      this.auth = auth;
+    });
   }
 
   login(){
-    this._af.auth.login();
+    this.af.auth.login();
+  }
+
+
+  logout(){
+    this.af.auth.logout();
   }
 
   ngOnInit(){
